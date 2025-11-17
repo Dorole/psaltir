44a1f014c74f5be98156d76af4a6b1d29c1f8f13
Dorotea <dorole@gmail.com>
2025-11-17 15:23:58 +0100

    Reading page - basic structure

--- lib/pages/reading_page.dart
+++ lib/pages/reading_page.dart
@@ -10,16 +10,76 @@ class ReadingPage extends StatelessWidget {
     final readingProvider = context.watch<ReadingProvider>();
 
     // ***** DEBUG VIEW *****
+
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text("*** READING PAGE ***"),
-          const SizedBox(height: 20),
-          Text("Reading choice: ${readingProvider.readingChoice}"),
-          Text("Category: ${readingProvider.category}"),
-          Text("Psalm number: ${readingProvider.psalmNumber}"),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const Text("*** READING PAGE ***"),
+            const SizedBox(height: 20),
+
+            Expanded(
+              child: Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 60.0),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            // DEBUG
+                            "Reading choice: ${readingProvider.readingChoice}",
+                          ),
+                          Text(
+                            "Category: ${readingProvider.category}",
+                          ), // DEBUG
+                          Text(
+                            "Psalm number: ${readingProvider.psalmNumber}",
+                          ), // DEBUG
+                          const SizedBox(height: 20),
+                          Text(
+                            // DEBUG
+                            textAlign: TextAlign.justify,
+                            softWrap: true,
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: AlignmentGeometry.centerLeft,
+                    child: SizedBox(
+                      width: 60,
+                      child: Center(
+                        child: IconButton(
+                          onPressed: () =>
+                              {}, //Connect to provider to get next psalm
+                          icon: Icon(Icons.chevron_left_rounded),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: AlignmentGeometry.centerRight,
+                    child: SizedBox(
+                      width: 60,
+                      child: Center(
+                        child: IconButton(
+                          onPressed: () =>
+                              {}, //Connect to provider to get next psalm
+                          icon: Icon(Icons.chevron_right_rounded),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
